eb178b84a9a42aaefc6d49ad91280cad250b37ea
nadiamartinmontesi <86934894+nadiamartinmontesi@users.noreply.github.com>
2023-06-26 23:01:45 -0300

    Feat getById handler

--- cmd/server/handler/dentist.go
+++ cmd/server/handler/dentist.go
@@ -1,1 +1,35 @@
 package handler
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/Laura-2950/desafio-final-go/internal/dentist"
+	"github.com/Laura-2950/desafio-final-go/pkg/web"
+	"github.com/gin-gonic/gin"
+)
+
+type DentistHandler struct {
+	DentistService dentist.IService
+}
+
+func (h *DentistHandler) GetById(ctx *gin.Context) {
+	idParam := ctx.Param("id")
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, web.NewBadRequestApiError("Invalid ID"))
+		return
+	}
+
+	dentistFounded, err := h.DentistService.GetDentistByID(id)
+	if err != nil {
+		if errApi, ok := err.(*web.ErrorApi); ok {
+			ctx.AbortWithStatusJSON(errApi.Status, errApi)
+			return
+		}
+		ctx.AbortWithStatusJSON(http.StatusInternalServerError, err)
+		return
+	}
+
+	ctx.JSON(http.StatusOK, &dentistFounded)
+}
--- internal/dentist/service.go
+++ internal/dentist/service.go
@@ -1,1 +1,21 @@
 package dentist
+
+import "github.com/Laura-2950/desafio-final-go/internal/domain"
+
+type IService interface {
+	GetDentistByID(id int) (*domain.Dentist, error)
+	// CreateNewDentists(dentist *domain.Dentist) (*domain.Dentist, error)
+	// DeleteDentist(id int) error
+}
+
+type Service struct {
+	Repository IRepository
+}
+
+func (s *Service) GetDentistByID(id int) (*domain.Dentist, error) {
+	dentist, err := s.Repository.GetByID(id)
+	if err != nil {
+		return nil, err
+	}
+	return dentist, nil
+}
