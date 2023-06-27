a0f3e01913c60ff716f1298046d61ced536591ae
nadiamartinmontesi <86934894+nadiamartinmontesi@users.noreply.github.com>
2023-06-27 10:00:08 -0300

    Update dentist.go

--- internal/domain/dentist.go
+++ internal/domain/dentist.go
@@ -1,35 +1,8 @@
-package handler
+package domain
 
-import (
-	"net/http"
-	"strconv"
-
-	"github.com/Laura-2950/desafio-final-go/internal/dentist"
-	"github.com/Laura-2950/desafio-final-go/pkg/web"
-	"github.com/gin-gonic/gin"
-)
-
-type DentistHandler struct {
-	DentistService dentist.IService
-}
-
-func (h *DentistHandler) GetById(ctx *gin.Context) {
-	idParam := ctx.Param("id")
-	id, err := strconv.Atoi(idParam)
-	if err != nil {
-		ctx.AbortWithStatusJSON(http.StatusBadRequest, web.NewBadRequestApiError("Invalid ID"))
-		return
-	}
-
-	dentistFounded, err := h.DentistService.GetDentistByID(id)
-	if err != nil {
-		if errApi, ok := err.(*web.ErrorApi); ok {
-			ctx.AbortWithStatusJSON(errApi.Status, errApi)
-			return
-		}
-		ctx.AbortWithStatusJSON(http.StatusInternalServerError, err)
-		return
-	}
-
-	ctx.JSON(http.StatusOK, &dentistFounded)
+type Dentist struct {
+	ID                 int    `json:"id"`
+	Name               string `json:"name" binding:"required"`
+	LastName           string `json:"last_name" binding:"required"`
+	RegistrationNumber string `json:"registration_number" binding:"required"`
 }
