8f9dbe22a7456727c75bf3ea67d0939b8932de8c
Laura <laura@duxsoftware.com.ar>
2023-06-30 20:47:44 -0300

    feat postman_collection

--- data/data.sql
+++ data/data.sql
@@ -8,26 +8,6 @@ CREATE TABLE IF NOT EXISTS dentists (
     registration_number VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE patients (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
-    address VARCHAR(255),
-    dni VARCHAR(20) NOT NULL,
-    registration_date VARCHAR(20) NOT NULL
-);
-
-CREATE TABLE shifts (
-    id INT NOT NULL AUTO_INCREMENT,
-    patient_id INT,
-    dentist_id INT,
-    date_hour VARCHAR(50) NOT NULL,
-	description VARCHAR(255) DEFAULT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (patient_id) REFERENCES patients(id),
-    FOREIGN KEY (dentist_id) REFERENCES dentists(id)
-);
-
 INSERT INTO dentists (name, last_name, registration_number) VALUES
 	('John', 'Doe', '12345'),
 	('Jane', 'Smith', '67890'),
@@ -50,6 +30,14 @@ INSERT INTO dentists (name, last_name, registration_number) VALUES
 	('Matthew', 'Moore', '78901'),
 	('Sofia', 'King', '89012');
 
+CREATE TABLE patients (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    address VARCHAR(255),
+    dni VARCHAR(20) NOT NULL,
+    registration_date VARCHAR(20) NOT NULL
+);
 
 INSERT INTO patients (name, last_name, address, dni, registration_date) VALUES
 	('John', 'Doe', '123 Main St', '123456789', '06/01/2023'),
@@ -73,12 +61,21 @@ INSERT INTO patients (name, last_name, address, dni, registration_date) VALUES
 	('Henry', 'Harris', '753 Oak St', '963741852', '06/04/2023'),
 	('Harper', 'Walker', '369 Birch St', '741852963', '06/02/2023');
 
-
+CREATE TABLE shifts (
+    id INT NOT NULL AUTO_INCREMENT,
+    patient_id INT,
+    dentist_id INT,
+    date_hour VARCHAR(50) NOT NULL,
+	description VARCHAR(255) DEFAULT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (patient_id) REFERENCES patients(id),
+    FOREIGN KEY (dentist_id) REFERENCES dentists(id)
+);
 INSERT INTO shifts (patient_id, dentist_id, date_hour, description) VALUES 
 	(9, 20, '04/07/2023 11:30', "consulta anual"),
 	(13, 3, '10/08/2023 18:45', "consulta anual"),
 	(1, 15, '04/08/2023 15:00', "consulta anual"),
 	(10, 5, '25/07/2023 18:00', "consulta anual"),
 	(13, 20,'10/08/2023 18:30', "consulta anual"),
-	(1, 5,'20/07/2023 11:30', "consulta anual"),
+	(9, 5,'20/07/2023 11:30', "consulta anual"),
 	(1, 18,'14/07/2023 08:00', "consulta anual");
