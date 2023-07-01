6ef36891fa80c9a9be56ca814deb2089d2c60147
nadiaMartinML <nadia.martin@mercadolibre.com>
2023-06-30 21:38:01 -0300

    Feat add authentication method

--- pkg/middleware/security.go
+++ pkg/middleware/security.go
@@ -1,1 +1,23 @@
 package middleware
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Authentication() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		token := c.GetHeader("TOKEN")
+		if token == "" {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, "token not found")
+			return
+		}
+		if token != os.Getenv("TOKEN") {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, "invalid token")
+			return
+		}
+		c.Next()
+	}
+}
